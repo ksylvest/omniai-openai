8110a986b0347b72c982c6df86696fd73005e5c9
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-19 23:14:35 -0700

    Support for Whisper text to speech

--- spec/omniai/openai/client_spec.rb
+++ spec/omniai/openai/client_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe OmniAI::OpenAI::Client do
     end
   end
 
+  describe '#transcribe' do
+    it 'proxies' do
+      allow(OmniAI::OpenAI::Transcribe).to receive(:process!)
+      client.transcribe('file.ogg')
+      expect(OmniAI::OpenAI::Transcribe).to have_received(:process!)
+    end
+  end
+
+  describe '#speak' do
+    it 'proxies' do
+      allow(OmniAI::OpenAI::Speak).to receive(:process!)
+      client.speak('Hello!')
+      expect(OmniAI::OpenAI::Speak).to have_received(:process!)
+    end
+  end
+
   describe '#connection' do
     it { expect(client.connection).to be_a(HTTP::Client) }
   end
