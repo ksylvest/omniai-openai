cb20f51f2e3d0f2ad8988acb0e1527b40d4cbbb9
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-21 11:12:37 -0700

    Support logging / timeout

--- lib/omniai/openai/config.rb
+++ lib/omniai/openai/config.rb
@@ -2,21 +2,36 @@
 
 module OmniAI
   module OpenAI
-    # Configuration for managing the OpenAI `api_key` / `organization` / `project` / `logger`.
+    # Configuration for OpenAI.
     class Config < OmniAI::Config
-      attr_accessor :organization, :project, :chat_options, :transcribe_options, :speak_options
-
       DEFAULT_HOST = 'https://api.openai.com'
 
-      def initialize
-        super
-        @api_key = ENV.fetch('OPENAI_API_KEY', nil)
-        @organization = ENV.fetch('OPENAI_ORGANIZATION', nil)
-        @project = ENV.fetch('OPENAI_PROJECT', nil)
-        @host = ENV.fetch('OPENAI_HOST', DEFAULT_HOST)
-        @chat_options = {}
-        @transcribe_options = {}
-        @speak_options = {}
+      # @!attribute [rw] organization
+      #   @return [String, nil] passed as `OpenAI-Organization` if specified
+      attr_accessor :organization
+
+      # @!attribute [rw] project
+      #   @return [String, nil] passed as `OpenAI-Organization` if specified
+      attr_accessor :project
+
+      # @param api_key [String, nil] optional - defaults to `ENV['OPENAI_API_KEY']`
+      # @param host [String, nil] optional - defaults to ENV['OPENAI_HOST'] w/ fallback to `DEFAULT_HOST`
+      # @param organization [String, nil] optional - defaults to `ENV['OPENAI_ORGANIZATION']`
+      # @param project [String, nil] optional - defaults to `ENV['OPENAI_PROJECT']`
+      # @param logger [Logger, nil] optional
+      # @param timeout [Integer, Hash, nil] optional
+      def initialize(
+        api_key: ENV.fetch('OPENAI_API_KEY', nil),
+        host: ENV.fetch('OPENAI_HOST', DEFAULT_HOST),
+        organization: ENV.fetch('OPENAI_ORGANIZATION', nil),
+        project: ENV.fetch('OPENAI_PROJECT', nil),
+        logger: nil,
+        timeout: nil
+      )
+        super(api_key:, host:, logger:, timeout:)
+
+        @organization = organization
+        @project = project
       end
     end
   end
--- lib/omniai/openai/version.rb
+++ lib/omniai/openai/version.rb
@@ -2,6 +2,6 @@
 
 module OmniAI
   module OpenAI
-    VERSION = '1.2.1'
+    VERSION = '1.3.0'
   end
 end
