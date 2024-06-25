3dc00eb4ac7ebd60ba6257b1fd1f8d847aeb9c7f
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-24 18:08:12 -0700

    Fix various yard issues

--- lib/omniai/openai/thread/runs.rb
+++ lib/omniai/openai/thread/runs.rb
@@ -31,12 +31,11 @@ module OmniAI
         end
 
         # @param assistant_id [String] required
-        # @param model [String] optional
-        # @param temperature [Float] optional
-        # @param instructions [String] optional
+        # @param model [String, nil] optional
+        # @param temperature [Float, nil] optional
+        # @param instructions [String, nil] optional
         # @param tools [Array<Hash>, nil] optional
         # @param metadata [Hash, nil] optional
-        # @param client [OmniAI::OpenAI::Client] optional
         # @return [OmniAI::OpenAI::Thread::Message]
         def build(assistant_id:, model: nil, temperature: nil, instructions: nil, tools: nil, metadata: {})
           Run.new(
