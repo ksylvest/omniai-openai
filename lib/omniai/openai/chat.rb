116f4de2f538bfb2db41273114ef9f1ae2ca4995
Kevin Sylvestre <kevin@ksylvest.com>
2024-07-18 14:28:59 -0700

    Support for GPT 4o mini

--- lib/omniai/openai/chat.rb
+++ lib/omniai/openai/chat.rb
@@ -16,6 +16,7 @@ module OmniAI
 
       module Model
         GPT_4O = 'gpt-4o'
+        GPT_4O_MINI = 'gpt-4o-mini'
         GPT_4 = 'gpt-4'
         GPT_4_TURBO = 'gpt-4-turbo'
         GPT_3_5_TURBO = 'gpt-3.5-turbo'
