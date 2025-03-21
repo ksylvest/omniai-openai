5aaf3b12a48e44b1e80c13570697291903e1f1f7
Kevin Sylvestre <kevin@ksylvest.com>
2025-03-21 10:29:34 -0700

    Support new speak voices / models
    This also swaps the default model to gpt-4o-mini-tts for the speak API.

--- lib/omniai/openai/speak.rb
+++ lib/omniai/openai/speak.rb
@@ -7,16 +7,24 @@ module OmniAI
       module Model
         TTS_1 = "tts-1"
         TTS_1_HD = "tts-1-hd"
+        GPT_4O_MINI_TTS = "gpt-4o-mini-tts"
       end
 
       module Voice
         ALLOY = "alloy" # https://platform.openai.com/docs/guides/text-to-speech/alloy
+        ASH = "ash" # https://platform.openai.com/docs/guides/text-to-speech/ash
+        BALLARD = "ballard" # https://platform.openai.com/docs/guides/text-to-speech/ballard
+        CORAL = "coral" # https://platform.openai.com/docs/guides/text-to-speech/coral
         ECHO = "echo" # https://platform.openai.com/docs/guides/text-to-speech/echo
         FABLE = "fable" # https://platform.openai.com/docs/guides/text-to-speech/fable
         NOVA = "nova" # https://platform.openai.com/docs/guides/text-to-speech/nova
         ONYX = "onyx" # https://platform.openai.com/docs/guides/text-to-speech/onyx
+        SAGE = "sage" # https://platform.openai.com/docs/guides/text-to-speech/sage
         SHIMMER = "shimmer" # https://platform.openai.com/docs/guides/text-to-speech/shimmer
       end
+
+      DEFAULT_MODEL = Model::GPT_4O_MINI_TTS
+      DEFAULT_VOICE = Voice::ALLOY
 
     protected
 
